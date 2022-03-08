b0d81fb9e12d2465b1a3005bd0a42bcfd7441782
artagracia <artagraciaclaud@gmail.com>
2022-03-08 09:12:03 +0700

    bicara maju

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,380 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+	<!---google fonts hear-->
+	<link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!--font awesome css-->
+	<link rel="stylesheet" href="css/all.min.css">
+	<!--animate css hear-->
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- light_case css -->
+	<link rel="stylesheet" href="css/lightcase.css">
+	<!--swiper css-->
+	<link rel="stylesheet" href="css/swiper.min.css">
+	<!-- shortcut image png is loaded -->
+	<link rel="shortcut icon" href="images/logo/seo.png">
+	<!-- style CSS is loaded -->
+    <link rel="stylesheet" href="css/style.css">
+
+	<title>Bicaramaju</title>
+  </head>
+  <body data-spy="scroll" class="p-0 style-4 overflow-auto">
+	<!-- preloader start here -->
+	<div class="preloader">
+		<div class="preloader-inner">
+			<div class="preloader-icon">
+				<span></span>
+				<span></span>
+			</div>
+		</div>
+	</div>
+	<!-- preloader ending here -->
+
+	<!-- mobile menu area start -->
+	<div class="mobile-menu style-4">
+		<nav class="mobile-header primary-menu d-lg-none">
+			<div class="header-logo">
+				<a href="index.html" class="logo">
+					<img src="#" alt="logo">
+				</a>
+			</div>
+			<div class="header-bar">
+				<span></span>
+				<span></span>
+				<span></span>
+			</div> 
+		</nav>
+		<nav class="menu">
+			<div class="mobile-menu-area d-lg-none">
+				<div class="mobile-menu-area-inner" id="scrollbar">
+					<div class="mobile-search">
+						<div class="search-inner">
+							<input type="text" placeholder="Search Here......"/>
+							<button type="submit">
+								<span>
+									<i class="fas fa-search"></i>
+								</span>
+							</button>
+						</div>
+					</div>
+					<ul class="m-menu">
+						<li>
+							<a href="index.html" class="active">Home</a>
+						</li>
+						<li>
+							<a href="#0">Event</a>
+							<ul class="m-submenu">
+								<li>
+									<a href="blog-left-sidebar.html">Seminar</a>
+								</li>
+								<li>
+									<a href="blog-right-sidebar.html">Workshop</a>
+								</li>
+								<li>
+									<a href="blog-single.html">Pelatihan</a>
+								</li>
+							</ul>
+						</li>
+						<li>
+							<a href="#0">Kelas</a>
+							<ul class="m-submenu">
+								<li><a href="shop-page.html">Shope Page</a></li>
+								<li><a href="shop-single.html">Shope Single</a></li>
+								<li><a href="shop-cart.html">shope Cart</a></li>
+							</ul>
+						</li>
+						<li>
+							<a href="contact.html">Contact Us</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+	</div>
+	<!-- mobile menu area end -->
+
+	<!-- header start -->
+	<header class="d-none d-lg-block">
+		<div class="bg-white app-header seo-header">
+			<div class="container-fluid">
+				<div class="header-item">
+					<div class="header-logo">
+						<a href="index.html">
+						</a>
+					</div>
+					<div class="header-menu">
+							<nav class="primary-menu">
+								<div class="main-menu-area">
+									<ul class="main-menu">
+										<li>
+											<a href="index.html">Home</a>
+										</li>
+										<li>
+											<a href="#about">Tentang Kami</a>
+										</li>
+										<li>
+											<a href="#">Event</a>
+											<ul class="submenu">
+												<li>
+													<a href="blog-left-sidebar.html">Seminar</a>
+												</li>
+												<li>
+													<a href="blog-right-sidebar.html">Workshop</a>
+												</li>
+												<li>
+													<a href="blog-single.html">Pelatihan</a>
+												</li>
+											</ul>
+										</li>
+										<li class="menu-holder">
+											<a href="#">Kelas</a>
+											<ul class="mega-menu">
+												<li>
+													<span class="submenu-title">Bisnis</span>
+													<ul>
+														<li><a href="">Digital Marketing</a></li>
+														</li>
+													</ul>
+												</li>
+												<li>
+													<span class="submenu-title">Single Product Style</span>
+													<ul>
+														<li><a href="#">Gallery Left</a></li>
+														<li><a href="#">Gallery Right</a>
+														</li>
+														<li><a href="#">Tab Style Left</a>
+														</li>
+														<li><a href="#">Tab Style Right</a>
+														</li>
+														<li><a href="#">Sticky Left</a></li>
+														<li><a href="#">Sticky Right</a></li>
+													</ul>
+												</li>
+												<li>
+													<span class="submenu-title">Single Product Type</span>
+													<ul>
+														<li><a href="#">Single Product</a></li>
+														<li><a href="#">Single Product Sale</a></li>
+														<li><a href="#">Single Product Group</a>
+														</li>
+														<li><a href="#">Single Product Variable</a>
+														</li>
+														<li><a href="#">Single Product Affiliate</a>
+														</li>
+														<li><a href="#">Single Product Slider</a>
+														</li>
+													</ul>
+												</li>
+												<li>
+													<span class="submenu-title">Shop Related Pages</span>
+													<ul>
+														<li><a href="#">My Account</a></li>
+														<li><a href="#">Login | Register</a></li>
+														<li><a href="#">Wishlist</a></li>
+														<li><a href="#">Cart</a></li>
+														<li><a href="#">Checkout</a></li>
+														<li><a href="#">Compare</a></li>
+													</ul>
+												</li>
+											</ul>
+										</li>
+									</ul>
+								</div>
+							</nav>
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
+	<!-- header end -->
+
+	<!-- banner area start -->
+	<div class="app-banner style-2 style-3">
+		<div class="container">
+			<div class="baner-wrapper">
+				<div class="banner-text">
+					<h2>Time to Rise</h2>
+					<p>
+						Dapatkan keterampilan baru dan kembangkan keterampilan lama dengan kegiatan dan kelas menarik  untuk meningkatkan prospek karir kamu.
+						Mulai belajar seru dan efektif dengan <b style="color: #176685;">Bicaramaju.id</b>
+					</p>
+				</div>
+				<div class="banner-thumb">
+					<img src="images/bg.jpg" alt="banner-image">
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- banner area ends -->
+
+	<!-- about service two area start -->
+	<div id="about"class="service-two style-1">
+		<div class="container">
+			<div class="row style-two align-items-center">
+				<div class="col-lg-6 col-12">
+					<div class="service-thumb">
+						<img src="images/pic-bg1.jpg" class="wow" alt="service-image">
+					</div>
+				</div>
+				<div class="col-lg-6 col-12">
+					<div class="post-heading">
+						<span>Tentang Kami</span>
+						<p>
+							<b style="color: #33b6ff;">Bicaramaju.id</b> merupakan tempat berpetualang untuk mengembangkan dan mencari keterampilan baru demi pengalaman yang lebih baik. Nikmati banyak kelas dan aktivitas menarik dari pelatihan, seminar, workshop, kelas online hingga kelas industri dengan tenaga profesional dalam bidangnya. 
+							Menjadi lebih profesional dengan <b style="color: #33b6ff;">Bicaramaju.id</b>
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- about service two area start -->
+
+
+	<!-- team area start -->
+	<div class="our-team">
+		<div class="container">
+			<div class="section-heading text-center wow">
+				<h3>Bicara dengan Kami</h3>
+				<p>
+					Temu sapa dengan tim anda yang akan membantu Anda untuk terus mengembangkan aktivitas dan kegiatan yang berkualitas serta kenyamanan Anda dalam menggunakan fasilitas kami. 
+				</p>
+			</div>
+			<div class="section-wrapper">
+				<div class="row justify-content-center">
+					<div class="col-xl-3 col-lg-4 col-md-6 col-12">
+						<div class="team-content text-center wow">
+							<div class="team-thumb">
+								<img src="images/team/pauki-picsay.png" alt="tema-image">
+							</div>
+							<div class="team-text">
+								<h4>Lucky Irawan</h4></a>
+								<p>Managing Director</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-xl-3 col-lg-4 col-md-6 col-12">
+						<div class="team-content text-center wow">
+							<div class="team-thumb">
+								<img src="images/team/eko-picsay.png" alt="tema-image">
+							</div>
+							<div class="team-text">
+								<h4>Eko Wigiantoro</h4></a>
+								<p>Head of Operational</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-xl-3 col-lg-4 col-md-6 col-12">
+						<div class="team-content text-center wow">
+							<div class="team-thumb">
+								<img src="images/team/agil-picsay.png" alt="tema-image">
+							</div>
+							<div class="team-text">
+								<h4>Agil Riyandi</h4></a>
+								<p>Graphic Designer</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-xl-3 col-lg-4 col-md-6 col-12">
+						<div class="team-content text-center wow">
+							<div class="team-thumb">
+								<img src="images/team/meli.jpg" alt="tema-image">
+							</div>
+							<div class="team-text">
+								<h4>Meliana</h4></a>
+								<p>Account Executive</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- team area ends -->
+
+	
+
+	<!-- footer area start -->
+	<footer>
+		<div class="footer-area pt-120 pb-60">
+			<div class="container">
+				<div class="row">
+					<div class="col-xl-4 col-lg-6 col-md-8">
+						<div class="footer-widget mb-40">
+							<div class="footer-text">
+								<img src="#" alt="">
+								<p style="font-family: montserrat; font-weight: Bold; margin-top: -35px; font-size: 52px; line-height: 175px; font-family: arial; color: #262626;">Bicaramaju</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-12 col-md">
+						<div class="footer-widget mb-40" style="margin-right: -46px;">
+							<h3 style="font-size: 21px; line-height: 1em; color: #33b6ff;">Alamat</h3>
+							<p>Jl. Pangkalan Batu No. 86A <br>Kel. Semplak, Kec. Bogor Barat,<br>Kota Bogor</p>
+						</div>
+					</div>
+					<div class="col-12 col-md">
+						<div class="footer-widget mb-40">
+							<h3 style="font-size: 21px; line-height: 1em; color: #33b6ff;">Kontak</h3>
+							<p class="center" style="font-size: 16px; width: 110%; margin-right: 17px;">
+								<img src="images/logo/telp.png" style="width: 20px;"> +62 821-1332-6780<br>
+								<img src="images/logo/mail.png" style="width: 20px; font-size: 10px;"> caraberpikirindonesia@gmail.com</p>
+						</div>
+					</div>
+					<div class="col-auto col-md">
+						<div class="footer-widget mb-40">
+							<div class="position-right"></div>
+								<h3 style="font-size: 21px; line-height: 1em; color: #33b6ff;">Ikuti Kami</h3>
+									<a href="https://www.facebook.com/siomay.abahuki"><img src="images/logo/facebook.png" style="width: 35px; margin-top: -4px;"></a>
+									<a href="https://www.instagram.com/siomay.bogor.abahuki/"><img src="images/logo/instagram.png" style="width: 43px; margin-top: -4px;"></a>
+									<a href="#"><img src="images/logo/whatsapp.png" style="width: 40px; margin-top: -4px;"></a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+	</footer>
+		
+	<div class="footer-bottom style-two">
+		<div class="container">
+			<div class="copy-right text-center">
+				<p>Copyright © 2021 Bicaramaju</p>
+			</div>
+		</div>
+	</div>
+	<!-- footer area ends -->
+
+	<!--scroll up-->
+	<div class="scroll-top style-5">
+		<div class="scrollToTop active">
+			<span>
+				<i class="fas fa-arrow-up"></i>
+			</span>
+		</div>
+	</div>
+	<!--scroll up-->
+
+    <!-- Optional JavaScript -->
+	<!-- jQuery first, then Bootstrap JS, then font-awosame, then lightcase Js, then image loaded & isotope Js, than swiper js -->
+    <script src="js/jquery.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+    <script src="js/fontawesome.min.js"></script>
+	<script src="js/jquery.waypoints.min.js"></script>
+	<script src="js/imagesloaded.pkgd.min.js"></script>
+	<script src="js/jquery.counterup.min.js"></script>
+	<script src="js/count-down.js"></script>
+	<script src="js/isotope-min.js"></script>
+	<script src="js/lightcase.js"></script>
+	<script src="js/swiper.min.js"></script>
+	<script src="js/theia-sticky-sidebar.js"></script>
+	<script src="js/wow.min.js"></script>
+	<script src="js/active.js"></script>
+
+  </body>
+</html>
